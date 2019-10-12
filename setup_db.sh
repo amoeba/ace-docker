f33e852038738baea13cf6f86def6fe6f705141d
Bryce Mecum <petridish@gmail.com>
2019-10-11 18:18:41 -0800

    Remove commented out portion of setup_db script

--- setup_db.sh
+++ setup_db.sh
@@ -11,11 +11,6 @@ docker exec -i ace_db_1 sh -c 'echo "create database ace_shard" | exec mysql'
 docker exec -i ace_db_1 sh -c 'exec mysql' < Database/Base/AuthenticationBase.sql
 docker exec -i ace_db_1 sh -c 'exec mysql' < Database/Base/ShardBase.sql
 
-# for f in Database/Updates/Authentication/*sql
-# do
-#   docker exec -i ace_database_1 sh -c 'exec mysql' < "$f"
-# done
-
 wget https://github.com/ACEmulator/ACE-World-16PY-Patches/releases/download/v0.9.131/ACE-World-Database-v0.9.131.sql.zip
 unzip ACE-World-Database-v0.9.131.sql.zip
 docker exec -i ace_db_1 sh -c 'exec mysql' < ACE-World-Database-v0.9.131.sql
